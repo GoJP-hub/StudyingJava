c6bb762f2f017b198bd0263e5b781ac6a1f0d5b0
GoJP-hub <gohiranojp@hotmail.co.jp>
2020-11-29 23:22:26 +0900

    modify: added Array to insert value/not related to calc yet

--- Calculator/src/action/CalculatorAction.java
+++ Calculator/src/action/CalculatorAction.java
@@ -1,11 +1,20 @@
 package action;
 
+import java.util.ArrayList;
+
 import module.CalculationModule;
 
 public class CalculatorAction {
 	public static void main(String[] args) {
 		CalculationModule calc = new CalculationModule();
+		//branch feature/./3
+		//calling normal ArrayList and inserting value
+		ArrayList<String> list = new ArrayList<String>();
+		list = calc.addValue(list, "10", "+");
+		System.out.println(list.get(0));
+		System.out.println(list.get(1));
 
+		//branch feature/./2
 		int sum = calc.add(4, 4);
 		int difference = calc.subtract(4, 4);
 		int product = calc.multiply(4, 4);
