c1463d46d4b5ba65336011d9f5b2b5ea89e598f7
GoJP-hub <gohiranojp@hotmail.co.jp>
2020-11-29 10:26:13 +0900

    modify: fixed conflict for feature/basic/2

--- Basic/src/sampleVariable/SampleVariable.java
+++ Basic/src/sampleVariable/SampleVariable.java
@@ -2,17 +2,19 @@ package sampleVariable;
 
 public class SampleVariable {
 public static void main(String[] args) {
-	//変数のサンプル
-	String str = "Schoo"; //文字列
-	char ch = 's'; //文字
-	int i = 100; //整数
-	float f = 3.14F;//実数
+	String str = "schoo";	// 文字列
+	char ch	= 's';	// 文字
+	int	 i	= 100;	// 整数
+	float f = 3.14F;// 実数
 
-	//変数の出力
-	System.out.println(str);
-	System.out.println(ch);
-	System.out.println(i);
-	System.out.println(f);
+	// 変数の内容を出力
+	System.out.println("文字列：" + str);
+	System.out.println("文字：" + ch);
+	System.out.println("整数：" + i);
+	System.out.println("実数：" + f);
 
+	//変数のカウント
+	int count = str.length();
+	System.out.println("the number for str is: " + count);
 }
 }
