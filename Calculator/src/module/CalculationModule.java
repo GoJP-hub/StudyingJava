724e99211d388f048320a49d2f4a4498ac77a1fc
GoJP-hub <gohiranojp@hotmail.co.jp>
2020-12-02 00:14:43 +0900

    modify: added valid error handling conditions for Input Processing

--- Calculator/src/module/CalculationModule.java
+++ Calculator/src/module/CalculationModule.java
@@ -1,17 +1,6 @@
 package module;
 
-import java.util.ArrayList;
-
 public class CalculationModule {
-	//branch feature/./3
-	//created arraylist for storign data #but only arraylist creation and not much on calc
-	public ArrayList<String> addValue(ArrayList<String> list, String num, String key){
-		list.add(num);
-		list.add(key);
-		return list;
-	}
-
-
 	//branch feature/./2
 	public int add(int i1, int i2) {
 		return i1 + i2;
--- Calculator/src/module/InputProcessModule.java
+++ Calculator/src/module/InputProcessModule.java
@@ -18,35 +18,63 @@ public class InputProcessModule {
 		//designed to insert number first, then keys by conditioning
 		String consoleKey = "";
 		int mode = 1;
-		boolean equalFlg = false;
+		boolean equationFlg = false;
+
+		//branch feature/./6
+		//variable for error handling
+		String msg = "";
 
 		while (true) {
 
 			try {
-				if(mode == 1) {
-					System.out.println("Please type number");
+				if (mode == 1) {
+					//branch feature/./6
+					//added a flexibility to return back if error
+					System.out.println(msg + "Please type number");
 					consoleKey = br.readLine();
 					System.out.println("Inserted value is: " + consoleKey);
 
-					mode = 2;
-				}else if(mode == 2) {
+					//branch feature/./6
+					//Assertion on the inserted value
+					try {
+						checkNum(consoleKey);
+					}catch(NumberFormatException e){
+						msg = "Invalid value.";
+						continue;
+					}
 
-					System.out.println("Please type key");
+					mode = 2;
+					msg = "";
+				} else if (mode == 2) {
+					//branch feature/./6
+					//added a flexibility to return back if error
+					System.out.println(msg + "Please type key");
 					consoleKey = br.readLine();
 					System.out.println("Inserted key is: " + consoleKey);
 
-					equalFlg = checkKey(consoleKey);
+					//branch feature/./5
+					//Assertion on the key: equal
+					equationFlg = checkKey(consoleKey);
 
+					//branch feature/./6
+					//Assertion on the key: +, -, *, /
+					if(checkKey(consoleKey, equationFlg)) {
+						msg = "Invalid value.";
+						continue;
+					}
 					mode = 1;
+					msg = "";
 				}
 
 				list.add(consoleKey);
 
-				if(equalFlg) {
+				if (equationFlg) {
 					break;
 				}
 			} catch (IOException e) {
-				e.printStackTrace();
+				//branch feature/./6
+				//if error is found, exit
+				System.exit(0);
 			}
 		}
 		return list;
@@ -54,11 +82,11 @@ public class InputProcessModule {
 
 	//branch feature/./5
 	//added assertion to check whether it is equal
-	public boolean checkKey(String key) {
-		boolean checkFlg = false;
-		switch (key) {
+	public boolean checkKey(String consoleKey) {
+		boolean checkEqual = false;
+		switch (consoleKey) {
 		case "=":
-			checkFlg = true;
+			checkEqual = true;
 			break;
 		case "+":
 		case "-":
@@ -68,6 +96,32 @@ public class InputProcessModule {
 		default:
 			System.out.println("please enter the key: +, -, *, /, =");
 		}
-		return checkFlg;
+		return checkEqual;
+	}
+
+	//branch feature/./6
+	//for more assertion and error handling; Number
+	public boolean checkNum(String consoleKey) throws NumberFormatException {
+		Integer.parseInt(consoleKey);
+		return true;
+	}
+
+	//branch feature/./6
+	//for more assertion and error handling: Equation key (distinguished by usage of flg)
+	public boolean checkKey(String consoleKey, boolean equationFlg) {
+		boolean checkEquation = false;
+
+		if(!equationFlg) {
+			switch(consoleKey) {
+			case "+":
+			case "-":
+			case "*":
+			case "/":
+				break;
+				default:
+					checkEquation = true;
+			}
+		}
+		return checkEquation;
 	}
 }
